4511d0fec23bef19af036b98a98de95277564ce6
ritajeong <litch0s2@gmail.com>
2021-11-30 13:52:00 +0900

    feat: 투두아이템 분리 props, class style binding

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+function TodoItem({ content, completed }){
+    return (
+      <li className={completed ? 'completed' : null}>
+        <div className="view">
+          <input className="toggle" type="checkbox" checked={completed} />
+          <label>{content}</label>
+          <button className="destroy"></button>
+        </div>
+        <input className="edit" value={content} />
+    </li>
+    )
+}
 
 export default function Main() {
   return (
@@ -6,14 +19,9 @@ export default function Main() {
         <input id="toggle-all" className="toggle-all" type="checkbox" checked={false} />
         <label htmlFor="toggle-all"></label>
         <ul className="todo-list">
-        <li className="">
-            <div className="view">
-            <input className="toggle" type="checkbox" checked={false} />
-            <label>투두 리스트</label>
-            <button className="destroy"></button>
-            </div>
-            <input className="edit" value="투두 리스트" />
-        </li>
+            <TodoItem content={"투두 리스트"} completed={false} />
+            <TodoItem content={"투두"} completed={false} />
+            <TodoItem content={"책 읽기"} completed={true} />
         </ul>
     </section>
   )
